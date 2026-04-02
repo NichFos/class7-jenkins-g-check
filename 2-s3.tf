ffc4909aa088304642b943cb010c2b553eddddc0
NichFos <nicholasdfoster645@gmail.com>
2026-04-02 02:18:34 -0500

    Added Jenkins user data source and added access policy to s3 bucket for Jenkins user

--- 2-s3.tf
+++ 2-s3.tf
@@ -30,8 +30,31 @@ resource "aws_s3_bucket_policy" "g_check_bucket_policy" {
         "Resource" : [
           "arn:aws:s3:::${aws_s3_bucket.g_check_bucket.id}/*"
         ]
-      },
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "jenkins_s3_access_policy" {
+  name        = "JenkinsS3AccessPolicy"
+  description = "Policy to allow Jenkins user to access S3 bucket"
+  policy      = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
       {
+        "Sid" : "AllowS3Access",
+        "Effect" : "Allow",
+        "Principal" : "*",
+        "Action" : [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket"
+        ],
+        "Resource" : [
+          "arn:aws:s3:::${aws_s3_bucket.g_check_bucket.id}",
+          "arn:aws:s3:::${aws_s3_bucket.g_check_bucket.id}/*"
+        ],
+      },{
         "Sid" : "AllowGetandPutBucketPolicy",
         "Effect" : "Allow",
         "Principal" : "*",
@@ -46,8 +69,6 @@ resource "aws_s3_bucket_policy" "g_check_bucket_policy" {
     ]
   })
 }
-
-
 resource "aws_s3_bucket_ownership_controls" "g_check_bucket_owner" {
 
   bucket = aws_s3_bucket.g_check_bucket.id
---
+++ data.tf
@@ -0,0 +1,3 @@
+data "aws_iam_user" "jenkins_user" {
+  user_name = "Jenkins"
+}
